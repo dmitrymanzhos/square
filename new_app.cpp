d406516073ec85cbf35ba20af18643d16da4613d
Dmitry Manzhos <dmitrijmanzos6482@gmail.com>
2023-08-23 13:07:20 +0300

    commit 5

--- new_app.cpp
+++ new_app.cpp
@@ -10,23 +10,31 @@ const double EPS = 1e-20; ///< погрешность
 const int INF = 999999; ///< значение для бесконечности корней
 
 int solve_square(double [], double []);
-void print_roots(double [] ,double [], int count);
+void print_roots(double [], double [], int count);
 void getline(char []);
 void get_coefs(double []);
 int compare_to_0 (double a);
+int one_test(double, double, double, double, double, int);
+int all_tests();
 
 
 /*!
 Точка входа
 */
-int main()
+int main(int arg_count, char *argv[])
 {
+    if (arg_count > 1) {
+        if (argv[1][0] == 't' && argv[1][1] == 'e' && argv[1][2] == 's' && argv[1][3] == 't') {
+            all_tests();
+            return 0;}
+        else return 0;}
+    else {
     double coefs[3] = {0};
     get_coefs(coefs);
     double roots[2] = {0};
     int count = solve_square(coefs, roots);
     print_roots(coefs, roots, count);
-    return 0;
+    return 0;}
 }
 
 /*!
@@ -49,9 +57,11 @@ int solve_square(double coefs[], double roots[])
         double d = coefs[1] * coefs[1] - 4 * coefs[0] * coefs[2];
         if (d <= -EPS)
             return 0;
-        else if (compare_to_0(d)) {
+        else if (compare_to_0(d))
+        {
             roots[0] = -coefs[1] / (2 * coefs[0]);
-            return 1;}
+            return 1;
+        }
         else if (d >= EPS) {
             roots[0] = (-coefs[1] + sqrt(d)) / (2 * coefs[0]);
             roots[1] = (-coefs[1] - sqrt(d)) / (2 * coefs[0]);
@@ -80,7 +90,7 @@ void print_roots(double coefs[] ,double roots[], int count)
                 break;}}
     else {
         switch (count) {
-        case 0: printf("нет решений\n");
+        case 0: printf("нет вещественных решений\n");
                 break;
         case 1: printf("1 корень:\n%.10lf\n", roots[0]);
                 break;
@@ -135,4 +145,25 @@ int compare_to_0 (double a)
     if (fabs(a) < EPS)
         return 1;
     else return 0;
+}
+
+int one_test(double a, double b, double c, double x1_ref, double x2_ref, int count_ref)
+{
+    double coefs[] = {a, b, c};
+    double roots[] = {0, 0};
+    int count = solve_square(coefs, roots);
+    if (count == count_ref)
+        if ((roots[0] == x1_ref && roots[1] == x2_ref) || (roots[1] == x1_ref && roots[0] == x2_ref)) {
+            printf("ok\n");
+            return 1;}
+    printf("FAIL: при coefs[%lf, %lf, %lf] roots[%lf, %lf] count = %i; ожидалось roots[%lf, %lf] count = %i\n",
+            coefs[0], coefs[1], coefs[2], roots[0], roots[1], count, x1_ref, x2_ref, count_ref);
+    return 0;
+}
+
+int all_tests()
+{
+    int total = 0;
+    total += one_test(0, 0, 0, 0, 0, INF);
+    return total;
 }
