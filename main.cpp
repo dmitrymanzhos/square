da9f04468c3bcbd26d0fbc05e32d27249ab0d4af
Dmitry Manzhos <dmitrijmanzos6482@gmail.com>
2023-08-24 16:26:10 +0300

    commit 14

--- main.cpp
+++ main.cpp
@@ -7,18 +7,18 @@
 */
 #include <stdio.h>
 #include <cstring>
-#include "all.h"
+#include "main.h"
 
 /*!
 Точка входа
 \param[in] arg_count количество аргументов командной строки
-\param[in] *argv[]
+\param[in] *argv[] список указателей на аргументы командной строки
 */
 int main(int arg_count, char *argv[])
     {
     if (arg_count > 1) ///< если есть аргументы командной строки кроме  имени файла
         {
-        if (strcmp(argv[1], "--test") == 0) ///< если 1й аргумент равен "test"
+        if (strcmp(argv[1], "--test") == 0) ///< если 1й аргумент равен "--test"
             {
             printf("тестов пройдено: %i\n", do_all_tests());
             return 0;
--- main.h
+++ main.h
@@ -1,18 +1,10 @@
 /*!
-\file all.h
+\file main.h
 Заголовочный файл с прототипами функций для main.cpp
 */
-struct test_data { ///< структура для 1 теста
-    double a, b, c;
-    double x1_ref, x2_ref;
-    int count_ref;
-    };
 
 int solve_square(double coefs[], double roots[]);
 void print_roots(double coefs[] ,double roots[], int count);
 void get_coefs(double coefs[]);
-void get_line(char line[]);
-int compare(double a, double b);
 
-int do_one_test(const test_data* test);
 int do_all_tests();
